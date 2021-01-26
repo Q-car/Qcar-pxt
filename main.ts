ec5a20fa6856b0ad48cf2ce9efaeef5cd66d8f9e
Qubot <1445788683@qq.com>
2021-01-26 14:55:28 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -85,10 +85,10 @@ namespace qcar {
     //% irstatus.fieldEditor="gridpicker" irstatus.fieldOptions.columns=2 
     //% weight=93 blockGap=8
 
-    export function IREnable(irstatus: irstatus): void {
-        if (irstatus == irstatus.iron) {
+    export function IREnable(IRstatus: irstatus): void {
+        if (IRstatus == irstatus.iron) {
             pins.digitalWritePin(DigitalPin.P14, 1)
-        } else if (irstatus == irstatus.iroff) {
+        } else if (IRstatus == irstatus.iroff) {
             pins.digitalWritePin(DigitalPin.P14, 0)
         } 
     }
